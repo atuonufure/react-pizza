4a3ff7aec2ee2e75a40e820b9e8f7afda7207bba
atuonufure <atuonufure@gmail.com>
2021-01-16 03:13:50 +0300

    add axios proptypes pizzablock component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
+import axios from 'axios';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
+
+  /* useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((resp) => resp.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []); */
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={() => <Home items={pizzas} />} />
         <Route exact path="/cart" component={Cart} />
       </div>
     </div>
