d3abd7716e9f3385398696a0e0addf30cc62dfda
atuonufure <atuonufure@gmail.com>
2021-01-18 04:32:14 +0300

    changes for heroku deploy

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -13,9 +13,9 @@ export const fetchPizzas = (sortBy, category) => (dispatch) => {
 
   axios
     .get(
-      `/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${
-        sortBy.order
-      }`,
+      `https://react-pizza-rtw.herokuapp.com/pizzas?${
+        category !== null ? `category=${category}` : ''
+      }&_sort=${sortBy.type}&_order=${sortBy.order}`,
     )
     .then(({ data }) => {
       dispatch(setPizzas(data));
